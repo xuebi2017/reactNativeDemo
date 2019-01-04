8b864325106e5f9123c212c796b70b3f7e0dc4b7
zyp <xuebi2013@yeah.net>
2019-01-04 18:17:07 +0800

    抽屉导航

--- App.js
+++ App.js
@@ -7,48 +7,105 @@
  */
 
 import React, { Component }from "react";
-import { Button, View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { createStackNavigator, createAppContainer, createDrawerNavigator } from "react-navigation";
 
-class HomeScreen extends Component {
+// class HomeScreen extends Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//         <Text>Home Screen</Text>
+//         <Button
+//         title="Go to Details"
+//         onPress={() => {return this.props.navigation.push('Details')}}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// class DetailsScreen extends Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//         <Text>Details Screen</Text>
+//         <Button
+//           title="Go back"
+//           onPress={() => this.props.navigation.goBack()}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// const AppNavigator = createStackNavigator(
+//   {
+//     Home: HomeScreen,
+//     Details: DetailsScreen
+//   },
+//   {
+//     initialRouteName: "Home"
+//   }
+// );
+class MyHomeScreen extends Component {
+  static navigationOptions = {
+    drawerLabel: 'Home',
+    // drawerIcon: ({ tintColor }) => (
+    //   <Image
+    //     source={require('./chats-icon.png')}
+    //     style={[styles.icon, {tintColor: tintColor}]}
+    //   />
+    // ),
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-        title="Go to Details"
-        onPress={() => {return this.props.navigation.push('Details')}}
-        />
-      </View>
+      <Button
+        onPress={() => {this.props.navigation.navigate('Notifications');}}//this.props.navigation.openDrawer();
+        title="Go to notifications"
+      />
     );
   }
 }
 
-class DetailsScreen extends Component {
+class MyNotificationsScreen extends Component {
+  static navigationOptions = {
+    drawerLabel: 'Notifications',
+    // drawerIcon: ({ tintColor }) => (
+    //   <Image
+    //     source={require('./notif-icon.png')}
+    //     style={[styles.icon, {tintColor: tintColor}]}
+    //   />
+    // ),
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
     );
   }
 }
 
-const AppNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
   },
-  {
-    initialRouteName: "Home"
-  }
-);
+});
 
-const AppContainer = createAppContainer(AppNavigator);
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+});
+
+const AppContainer = createAppContainer(MyDrawerNavigator);
 export default class App extends Component {
   render() {
     return <AppContainer />;
