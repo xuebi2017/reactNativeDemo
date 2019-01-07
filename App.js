41b5a130e67b4140102588e397e652c882efb91a
zyp <xuebi2013@yeah.net>
2019-01-07 22:03:12 +0800

    react navigation 学习

--- App.js
+++ App.js
@@ -7,105 +7,39 @@
  */
 
 import React, { Component }from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
 import { createStackNavigator, createAppContainer, createDrawerNavigator } from "react-navigation";
+import DetailsScreen from './src/views/details/details'
+import HomeScreen from './src/views/home/home'
 
-// class HomeScreen extends Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//         <Text>Home Screen</Text>
-//         <Button
-//         title="Go to Details"
-//         onPress={() => {return this.props.navigation.push('Details')}}
-//         />
-//       </View>
-//     );
-//   }
-// }
+//忽略黄色警告
+console.disableYellowBox = true;
 
-// class DetailsScreen extends Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//         <Text>Details Screen</Text>
-//         <Button
-//           title="Go back"
-//           onPress={() => this.props.navigation.goBack()}
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-// const AppNavigator = createStackNavigator(
-//   {
-//     Home: HomeScreen,
-//     Details: DetailsScreen
-//   },
-//   {
-//     initialRouteName: "Home"
-//   }
-// );
-class MyHomeScreen extends Component {
-  static navigationOptions = {
-    drawerLabel: 'Home',
-    // drawerIcon: ({ tintColor }) => (
-    //   <Image
-    //     source={require('./chats-icon.png')}
-    //     style={[styles.icon, {tintColor: tintColor}]}
-    //   />
-    // ),
-  };
-
-  render() {
-    return (
-      <Button
-        onPress={() => {this.props.navigation.navigate('Notifications');}}//this.props.navigation.openDrawer();
-        title="Go to notifications"
-      />
-    );
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+      fontWeight: 'bold',
+      },
+    }
   }
-}
+);
 
-class MyNotificationsScreen extends Component {
-  static navigationOptions = {
-    drawerLabel: 'Notifications',
-    // drawerIcon: ({ tintColor }) => (
-    //   <Image
-    //     source={require('./notif-icon.png')}
-    //     style={[styles.icon, {tintColor: tintColor}]}
-    //   />
-    // ),
-  };
+//调试app时刷新应该停留在当前页，实际上不是的
+const navigationPersistenceKey = __DEV__ ? "NavigationStateDEV" : null;
+// export const App = () => <AppNavigator persistenceKey={navigationPersistenceKey} />;
 
-  render() {
-    return (
-      <Button
-        onPress={() => this.props.navigation.goBack()}
-        title="Go back home"
-      />
-    );
-  }
-}
+const AppContainer = createAppContainer(AppNavigator);
 
-const styles = StyleSheet.create({
-  icon: {
-    width: 24,
-    height: 24,
-  },
-});
 
-const MyDrawerNavigator = createDrawerNavigator({
-  Home: {
-    screen: MyHomeScreen,
-  },
-  Notifications: {
-    screen: MyNotificationsScreen,
-  },
-});
-
-const AppContainer = createAppContainer(MyDrawerNavigator);
 export default class App extends Component {
   render() {
     return <AppContainer />;
---
+++ src/views/details/details.js
@@ -0,0 +1,39 @@
+import React, { Component }from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+
+export default class DetailsScreen extends Component {
+  // static navigationOptions = {
+    // title: '详情页',
+    
+  // };
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('otherParam', '详情页'),
+      headerRight: (
+        <Button
+          onPress={() => alert('This is a button!')}
+          title="Info"
+          color="#fff"
+        />
+      ),
+    }
+  }
+    render() {
+        const  { navigation }  = this.props;
+        console.log('this.props',this.props)
+        console.log('navigation', navigation)
+        const  itemId = navigation.getParam('itemId', 'NO-ID');
+        const otherParam = navigation.getParam('otherParam', 'some default value')
+      return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <Text>Details Screen</Text>
+          <Text>itemId:{JSON.stringify(itemId)}</Text>
+          <Text>otherParam:{JSON.stringify(otherParam)}</Text>
+          <Button
+            title="Go back"
+            onPress={() => this.props.navigation.goBack()}
+          />
+        </View>
+      );
+    }
+  }
